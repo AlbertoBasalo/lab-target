9ba3b246790bb39e89a7df3edb11314febdfa6db
Alberto Basalo <albertobasalo@hotmail.com>
2024-06-09 18:25:47 +0200

    feat: add interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { provideHttpClient, withFetch, withInterceptors } from "@angular/common/http";
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
+import { metricsInterceptor } from "lab-core";
 import { provideCrudRepository } from "lab-srv";
 import { environment } from "../environments/environment";
 import { routes } from "./app.routes";
@@ -9,7 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptors([])),
+    provideHttpClient(withFetch(), withInterceptors([metricsInterceptor])),
     provideCrudRepository(environment.apiUrl),
   ],
 };
