e34b6b8e879170d613602724178f350f2d819611
Alberto Basalo <albertobasalo@hotmail.com>
2024-06-11 09:31:05 +0200

    feat: no let

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
-import { AsyncPipe, JsonPipe } from "@angular/common";
+import { AsyncPipe } from "@angular/common";
 import { Component, inject } from "@angular/core";
 import { LocationComponent } from "@lab/ui";
+import { Observable } from "rxjs";
 import { AppService } from "./app.service";
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [LocationComponent, AsyncPipe, JsonPipe],
+  imports: [LocationComponent, AsyncPipe],
   template: `
     <h1>Welcome to {{ title }}!</h1>
-    <lab-ui-location value="Mars" />
-    <pre>{{ activities$ | async | json }}</pre>
+    <section>
+      <!-- @let activities = activities$ | async; -->
+      @if( activities$ | async; as activities){
+      <ul>
+        @for(activity of activities; track activity){
+        <li>
+          <span>
+            <a href="">{{ activity.name }}</a>
+          </span>
+          <lab-ui-location [value]="activity.location" />
+        </li>
+        }
+      </ul>
+      }
+    </section>
   `,
 })
 export class AppComponent {
   title = "lab-target";
   #service: AppService = inject(AppService);
-  activities$ = this.#service.getActivities$();
+  activities$: Observable<any[]> = this.#service.getActivities$();
 }
